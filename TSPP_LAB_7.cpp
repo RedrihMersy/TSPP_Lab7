dfdfb6febf3260b4cda6fb1ed51b85b0518415fb
Yarosh_Anton <esk.m@list.ru>
2018-05-19 18:41:49 +0300

    Start the project

---
+++ TSPP_LAB_7.cpp
@@ -0,0 +1,12 @@
+// TSPP_LAB_7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL,"Russian");
+	return 0;
+}
+
