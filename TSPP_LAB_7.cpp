ebebfd72fe6c37934d37eae8bbe29c07adb6b7c8
Yarosh_Anton <esk.m@list.ru>
2018-05-19 23:23:50 +0300

    Add clss ClientOfBank and started add class Manager

--- TSPP_LAB_7.cpp
+++ TSPP_LAB_7.cpp
@@ -3,6 +3,74 @@
 
 #include "stdafx.h"
 #include "iostream"
+#include <Windows.h>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+struct pd {
+	string FIO;
+	string number;
+	string given;
+};
+
+class ClientOfTheBank {
+public:
+	void GiveInformation(pd *PasportData, int *Id, double *BalanceOfMoney, int & Agreement);
+	bool makeAgreement(pd *PasportData, int *Id, double *BalanceOfMoney, int & Agreement);
+	ClientOfTheBank() {
+		Id = 1;
+		Agreement = 0;
+		BalanceOfMoney = 0;
+		pasportData.FIO = "Pupkin Petr Petrovich";
+		pasportData.number = "123 98745";
+		pasportData.given = "Rf. city Velikiy Novgorod";
+		cout << " | Создан объект клинета банка" << endl;
+	}
+	ClientOfTheBank(int cod, int agreement, int zmoney, string fio, string number, string given) {
+		Id = cod;
+		Agreement = agreement;
+		BalanceOfMoney = zmoney;
+		pasportData.FIO = fio;
+		pasportData.number = number;
+		pasportData.given = given;
+		cout << " | Создан объект клинета банка" << endl;
+	}
+private:
+	pd pasportData;
+	double BalanceOfMoney;
+	int Id;
+	int Agreement;
+};
+
+void ClientOfTheBank::GiveInformation(pd *PasportDates, int *Ides, double *BalanceOfMones, int & Agreementes) {
+	*Ides = Id;
+	*BalanceOfMones = BalanceOfMoney;
+	Agreementes = Agreement;
+	PasportDates->FIO = pasportData.FIO;
+	PasportDates->given = pasportData.given;
+	PasportDates->number = pasportData.number;
+
+}
+
+bool ClientOfTheBank::makeAgreement(pd *PasportData, int *Id, double *BalanceOfMoney, int & Agreement) {
+
+}
+
+
+class Manager {
+public:
+	bool AddDataOfClint(ListOfClient Object);
+	bool giveDataOfClint(ListOfClient Object);
+	bool conclusionAgrement(<T> ObjecOfAgreement);
+	bool setMessageToPayMasterForDelivery(ClientOfTheBank Client, <T> ObjecOfAgreement);
+	bool setMessageToPayMasterForReseptiomMoney(ClientOfTheBank Client, <T> ObjecOfAgreement);
+private:
+	int amountOfCreditCard;
+	int amountOfDebitCard;
+	int amountOfPayCard;
+};
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
